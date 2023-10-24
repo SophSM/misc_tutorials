9d4f4864d5bd88346bacceb19b7b7d50f053f37b
Sofía Salazar <sogusama02@gmail.com>
2023-10-23 20:18:08 -0600

    index script

---
+++ 03_STAR_pipeline/scripts/qc1.sh
@@ -0,0 +1,2 @@
+# sustituye los paths con los tuyos
+for file in /mnt/Citosina/amedina/ssalazar/claseSTAR/data/*.fastq.gz; do fastqc $file -o /mnt/Citosina/amedina/ssalazar/claseSTAR/quality1; done
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. How to create a github repo (Spanish)
 2. Introduction to R and RStudio (Spanish)
+3. RNA-seq with STAR tutorial (Spanish)
